48acb2a4978f9026246cef29e353aaa3529930f2
Anwar Jamis <anwarjamis@gmail.com>
2022-05-04 15:59:57 -0400

    simple airbnb w/no style

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,47 @@
 class FlatsController < ApplicationController
+  before_action :find_flat, only: %i[show create edit update destroy]
+
   def index
     @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @flat.destroy
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+  def find_flat
+    @flat = Flat.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,13 @@
 ActiveRecord::Schema.define(version: 2022_05_04_181435) do
 
   create_table "flats", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.text "description"
-    t.integer "price_per_night"
-    t.integer "number_of_guests"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string 'name'
+    t.string 'address'
+    t.text 'description'
+    t.integer 'price_per_night'
+    t.integer 'number_of_guests'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
 end
