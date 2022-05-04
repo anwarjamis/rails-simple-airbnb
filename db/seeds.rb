18963d0c57c4a9e83d184a50e8fa7ceded9d4853
Anwar Jamis <anwarjamis@gmail.com>
2022-05-04 14:56:23 -0400

    index done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: "Ferrari's Big 7",
+  address: '10 Kings Beach, Lake Tahoe, California, US',
+  description: 'A lovely motel room. Double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 120,
+  number_of_guests: 2
+)
